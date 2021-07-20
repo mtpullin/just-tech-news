3f143c1331fb817b1983b3905fac155af17c247e
mtpullin <mtpullin@gmail.com>
2021-07-20 17:44:34 -0400

    finished 14.2.7

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,48 +1,46 @@
-const router = require('express').Router()
-const {Comment} = require('../../models')
-const { post } = require('./post-routes')
+const router = require('express').Router();
+const { Comment } = require('../../models');
 
-router.get('/', (req,res) => {
-    Comment.findAll({
-
-    })
+router.get('/', (req, res) => {
+  Comment.findAll()
     .then(dbCommentData => res.json(dbCommentData))
     .catch(err => {
-        console.log(err);
-        res.status(500).json(err)
-    })
-})
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
-router.post('/',(req,res)=> {
-    Comment.create({
-        comment_text: req.body.comment_text,
-        user_id: req.body.user_id,
-        post_id: req.body.post_id
-    })
+router.post('/', (req, res) => {
+  // expects => {comment_text: "This is the comment", user_id: 1, post_id: 2}
+  Comment.create({
+    comment_text: req.body.comment_text,
+    user_id: req.session.user_id,
+    post_id: req.body.post_id
+  })
     .then(dbCommentData => res.json(dbCommentData))
     .catch(err => {
-        console.log(err);
-        res.json(400).json(err)
-    })
-})
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
 
-router.delete('/', (req,res)=> {
-    Comment.destroy({
-        where: {
-            id: req.params.id
-        }
-    })
+router.delete('/:id', (req, res) => {
+  Comment.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
     .then(dbCommentData => {
-        if(!dbCommentData){
-            res.status(404).json({message: 'no comment with this id'})
-            return;
-        }
-        res.json(dbCommentData)
+      if (!dbCommentData) {
+        res.status(404).json({ message: 'No comment found with this id!' });
+        return;
+      }
+      res.json(dbCommentData);
     })
     .catch(err => {
-        console.log(err);
-        res.status(500).json(err)
-    })
-})
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 module.exports = router;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,39 +1,51 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Post, User, Comment } = require('../models');
-router.get('/', (req,res)=> {
-Post.findAll({
+const { Post, User, Comment, Vote } = require('../models');
+
+// get all posts for homepage
+router.get('/', (req, res) => {
+  console.log('======================');
+  Post.findAll({
     attributes: [
-        'id',
-        'post_url',
-        'title',
-        'created_at',
-        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
     ],
     include: [
-        {
-            model: Comment,
-            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-            include: {
-                model: User,
-                attributes: ['username']
-            }
-        },
-        {
-            model: User,
-            attributes: ['username']
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
         }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
     ]
-})
-.then(dbPostData => {
-    //pass single post object into teh homepage template
-    const posts = dbPostData.map(post=> post.get({plain:true}))
-    res.render('homepage', {posts});
-})
-.catch(err => {
-    console.log(err);
-    res.status(500).json(err)
+  })
+    .then(dbPostData => {
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+
+      res.render('homepage', { posts });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
     });
-})
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
 
 module.exports = router;
